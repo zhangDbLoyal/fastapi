6f802bd5cb699e3704e7b745b6ecf804e7ff2d6f
zhang <17604767415@163.com>
2024-01-10 09:39:38 +0800

    test3

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print('hahhah')  # Press Ctrl+F8 to toggle the breakpoint.
+    print('test3')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
